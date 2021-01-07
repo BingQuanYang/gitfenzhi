eb6610ddd1f3c46051475f638b43ab93a57de9fe
ybq <hello@ybq.com>
2021-01-07 23:24:44 +0800

    init

---
+++ src/main/java/com/example/gitfenzhi/GitfenzhiApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitfenzhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitfenzhiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitfenzhiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitfenzhi/GitfenzhiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitfenzhi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitfenzhiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
